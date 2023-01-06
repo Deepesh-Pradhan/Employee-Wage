893e078afd6d4804d93acdbaf541ea73bd9025e8
deepeshpradhan <dpradhan0610@gmail.com>
2023-01-06 12:04:50 +0530

    Employeewage UC2 employee wage calculate

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -6,14 +6,20 @@ public class EmployeeWage {
         System.out.println("    WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM  ");
 
         int fullTime = 1;
+        int empWage_Per_Hour = 20;
+        int empHours = 0;
+        int empWage = 0;
         double randomcheck = Math.floor(Math.random() * 10) % 2;
 
         if(randomcheck == fullTime){
             System.out.println("Employee is Present");
+           empHours = 8;
         }
         else {
             System.out.println("Employee is Absent");
+            empHours = 0;
         }
-
+        empWage = empWage_Per_Hour * empHours;
+        System.out.println("Employee wage is : "+empWage);
     }
 }
