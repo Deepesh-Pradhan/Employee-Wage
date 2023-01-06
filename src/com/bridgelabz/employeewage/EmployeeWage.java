f46f3f4a2214a73721360a46d7538106b1dd2a02
deepeshpradhan <dpradhan0610@gmail.com>
2023-01-06 12:59:02 +0530

    Employeewage UC2 Employee Wage solving using swtich case

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,29 +1,30 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
+
+    static int empWagePerHour = 20;
+    static int empFullDayHours = 8;
+    static int empPartTimeHours = 4;
+
     public static void main(String[] args) {
 
         System.out.println("    WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM  ");
 
-        int fullTime = 1;
-        int partTime = 2;
-        int empWagePerHour = 20;
-        int empFullDayHours = 8;
-        int empPartTimeHours = 4;
-        int empWage = 0;
-        double randomcheck = Math.floor(Math.random() * 10) % 3;
 
-        if (randomcheck == fullTime) {
-            System.out.println("Employee is Present for Full time");
-            System.out.println("Employee`s Daily Wage is : " + empWagePerHour * empFullDayHours);
+        int randomcheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        } else if (randomcheck == partTime) {
-            System.out.println("Employee is Present for Part time");
-            System.out.println("Employee`s Daily Wage is : " + empWagePerHour * empPartTimeHours);
 
-        } else {
-            System.out.println("Employee is Absent");
+        switch (randomcheck) {
+            case 1 -> {
+                System.out.println("Employee is Present for Full time");
+                System.out.println("Employee`s Daily Wage is : " + empWagePerHour * empFullDayHours);
+            }
+            case 2 -> {
+                System.out.println("Employee is Present for Part time");
+                System.out.println("Employee`s Daily Wage is : " + empWagePerHour * empPartTimeHours);
+            }
+            default -> System.out.println("Employee is Absent");
         }
-
+        }
     }
-}
+
