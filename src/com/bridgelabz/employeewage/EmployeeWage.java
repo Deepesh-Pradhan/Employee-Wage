d0a00020d1cad1be79c4770962a9deeb84f80023
deepeshpradhan <dpradhan0610@gmail.com>
2023-01-06 10:39:57 +0530

    Employeewage Welcom message

---
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+
+        System.out.println("    WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM  ");
+
+    }
+}
